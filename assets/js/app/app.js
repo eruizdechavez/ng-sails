c5ff1035340e10def712313207a11d1c8d812800
Erick Ruiz de Chavez <erickrdch@gmail.com>
2014-02-07 07:21:13 -0500

    Fix remaining references from previous project

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,10 +1,9 @@
 define([
     'angular',
     'text!templates/header.html',
-    'welcome/welcome',
-    'flags/flags'
+    'welcome/welcome'
 ], function (ng, headerHtml) {
-    ng.module('app', ['flags'])
+    ng.module('app', ['welcome'])
         .directive('header', function () {
             return {
                 restrict: 'A',
